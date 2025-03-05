5493b8d55a0d70a8d5460f6f3c147cdae01503d9
JunJie Ma <mjj1428@126.com>
2025-03-05 18:36:54 +0800

    Update page layout

--- javascripts/scripts/get.time.js
+++ javascripts/scripts/get.time.js
@@ -5,12 +5,16 @@ function updateTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const year = String(now.getFullYear());
     const timezoneOffset = now.getTimezoneOffset();
     const offsetHours = String(Math.abs(Math.floor(timezoneOffset / 60))).padStart(2, '0');
     const offsetMinutes = String(Math.abs(timezoneOffset % 60)).padStart(2, '0');
     const offsetSign = timezoneOffset > 0 ? '-' : '+';
-    const formattedTime = `${hours}:${minutes}:${seconds}${' '}${dayOfWeek}${' '}(UTC${offsetSign}${offsetHours}:${offsetMinutes})`;
+    //const formattedTime = `${hours}:${minutes}:${seconds}${' '}${dayOfWeek}${' '}(UTC${offsetSign}${offsetHours}:${offsetMinutes})`;
+    const formattedTime = `${hours}:${minutes}${' '}${year}/${month}/${day}${' '}${dayOfWeek}`;
     document.getElementById('time').textContent = formattedTime;
 }
-setInterval(updateTime, 1000);
+setInterval(updateTime, 10000);
 updateTime();
