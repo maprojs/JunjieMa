abb586ad76d28cf82ccfc724525b120235e374bc
JunJie Ma <mjj1428@126.com>
2025-03-05 18:47:12 +0800

    Update

--- javascripts/scripts/get.time.js
+++ javascripts/scripts/get.time.js
@@ -4,17 +4,11 @@ function updateTime() {
     const dayOfWeek = daysOfWeek[now.getDay()];
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
     const day = String(now.getDate()).padStart(2, '0');
     const month = String(now.getMonth() + 1).padStart(2, '0');
     const year = String(now.getFullYear());
-    const timezoneOffset = now.getTimezoneOffset();
-    const offsetHours = String(Math.abs(Math.floor(timezoneOffset / 60))).padStart(2, '0');
-    const offsetMinutes = String(Math.abs(timezoneOffset % 60)).padStart(2, '0');
-    const offsetSign = timezoneOffset > 0 ? '-' : '+';
-    //const formattedTime = `${hours}:${minutes}:${seconds}${' '}${dayOfWeek}${' '}(UTC${offsetSign}${offsetHours}:${offsetMinutes})`;
     const formattedTime = `${hours}:${minutes}${' '}${year}/${month}/${day}${' '}${dayOfWeek}`;
     document.getElementById('time').textContent = formattedTime;
 }
-setInterval(updateTime, 10000);
+setInterval(updateTime, 60000);
 updateTime();
