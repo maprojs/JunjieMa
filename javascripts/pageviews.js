b514c924c4a943c46d3e43046a55edad850ccf6a
Junjie Ma <mjj1428@126.com>
2025-09-30 19:30:45 +0800

    Update pageviews.js

--- javascripts/pageviews.js
+++ javascripts/pageviews.js
@@ -18,10 +18,10 @@ function displayLastUpdatedData() {
   }
   HomeElement.innerHTML=`${iconViews} <a href="${analyticsFile}" target="_blank" class="black-link">Pageviews: ${viewsHome}</a> (by ${analyticsLink})`;
   if (ResearchElement) {
-    ResearchElement.innerHTML=`${iconViews}${' '}<a href="${analyticsFile}" target="_blank" class="black-link">Pageviews: ${viewsResearch}${' '}(by ${analyticsLink})`;
+    ResearchElement.innerHTML=`${iconViews} <a href="${analyticsFile}" target="_blank" class="black-link">Pageviews: ${viewsResearch}${' '}(by ${analyticsLink})`;
   }
   if (EducationElement) {
-    EducationElement.innerHTML=`${iconViews}${' '}<a href="${analyticsFile}" target="_blank" class="black-link">Pageviews: ${viewsEducation}${' '}(by ${analyticsLink})`;
+    EducationElement.innerHTML=`${iconViews} <a href="${analyticsFile}" target="_blank" class="black-link">Pageviews: ${viewsEducation}${' '}(by ${analyticsLink})`;
   }
 }
 
