7b67fdf1369a84d7abc47d80f3de1723291d516e
JunJie Ma <mjj1428@126.com>
2025-10-16 22:25:02 +0800

    Update modal.js

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -1,9 +1,11 @@
 function showModal(modalId) {
   document.getElementById(modalId).style.display = 'flex';
+  document.body.style.overflow = 'hidden';
 }
 
 function closeModal(modalId) {
   document.getElementById(modalId).style.display = 'none';
+  document.body.style.overflow = '';
 }
 
 function showModalWithFile(modalId, outTextId, fileName) {
