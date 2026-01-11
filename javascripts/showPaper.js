75a783ae197f73499dc16e94694c14a7f1511c79
Junjie Ma <mjj1428@126.com>
2026-01-12 05:23:23 +0800

    Update

---
+++ javascripts/showPaper.js
@@ -0,0 +1,19 @@
+let showAllPapers = false;
+
+function toggleFirstAuthor() {
+  const firstPapers = document.querySelectorAll('.firstPaper');
+  const otherPapers = document.querySelectorAll('.otherPaper');
+  const filterBtn = document.getElementById('paperFilter');
+
+  if (!showAllPapers) {
+    otherPapers.forEach(p => p.style.display = 'none');
+    firstPapers.forEach(p => p.style.display = 'block');
+    filterBtn.innerText = 'allPapers';
+    showAllPapers = true;
+  } else {
+    otherPapers.forEach(p => p.style.display = 'block');
+    firstPapers.forEach(p => p.style.display = 'block');
+    filterBtn.innerText = '1stPapers';
+    showAllPapers = false;
+  }
+}
